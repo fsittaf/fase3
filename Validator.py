c3668260aabc158788878590915e8422d4b1db86
Fernando <fsittaf@outlook.com>
2022-05-20 13:40:53 -0300

    error messages

--- Validator.py
+++ Validator.py
@@ -10,15 +10,21 @@ class Validator:
 
     def validate(self, user: User):
         if user.name == '':
-            raise Exception('Name cannot be empty')
+            # raise Exception('Name cannot be empty') -> antigo
+            print('Name cannot be empty')
+            return
         elif user.last_name == '':
-            raise Exception('Last Name cannot be empty')
+            print('Last Name cannot be empty')
+            return
         elif not self.is_email_valid(user.email):
-            raise Exception('Invalid email')
+            print('Invalid email')
+            return
         elif not self.is_age_valid(user.age):
-            raise Exception('Invalid age')
+            print('Invalid age')
+            return
         elif user.role not in Role._member_names_:
-            raise Exception('Invalid role')
+            print('Invalid role')
+            return
 
     def is_age_valid(self, age: str) -> bool:
         try:
